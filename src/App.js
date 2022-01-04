06903df2fca6af3f230b06d064405ebf4c5aa900
Marcos Lopes <marcos.eddul@gmail.com>
2022-01-03 21:36:58 -0300

    all changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,98 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from "react-tsparticles";
+import React, { Component} from 'react';
 
+const particlesOptions = {
+  fpsLimit: 60,
+  interactivity: {
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          options={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import brain from './brain.png'
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+            <div className="Tilt-inner pa3"><img src={brain} syle={{paddingTop: '5px'}} alt='logo' />  </div>
+        </Tilt>
         </div>
     );
 }
