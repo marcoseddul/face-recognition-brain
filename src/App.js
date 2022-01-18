02147246ecd92685e2eba811c2333da0c997997f
Marcos Lopes <marcos.eddul@gmail.com>
2022-01-17 22:10:42 -0300

    all ok

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Particles from "react-tsparticles";
 import React, { Component } from 'react';
 import Clarifai from 'clarifai';
 
+
 const app = new Clarifai.App({
   apiKey: '8ec97c58891449d29a169ebcd8e51449'
 });
@@ -85,8 +86,29 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -116,33 +138,26 @@ class App extends Component {
     app.models.predict(Clarifai.FACE_DETECT_MODEL,
       this.state.input)
       .then(response => {
-        this.displayFaceBox(this.calculateFaceLocation(response))
-
-        /*if (response) {
-          fetch('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', {
+        if (response) {
+          fetch('http://localhost:3000/image', {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               id: this.state.user.id
             })
-          })
-            .then(response => console.log(response.json()))
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-  
+          }).then(response => response.json())
+            .then(count => this.setState(Object.assign(this.state.user, {entries: count})))
         }
-          this.displayFaceBox(this.calculateFaceLocation(response))*/
+        this.displayFaceBox(this.calculateFaceLocation(response))
       })
-    //.catch(err => console.log(err));
   }
   onRouteChange = (route) => {
     if (route === 'signinout') {
-      this.setState({isSignedIn: false})
-    } else if (route === 'home'){
-      this.setState({isSignedIn: true})
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
     }
-    this.setState({route: route});
+    this.setState({ route: route });
   }
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
@@ -151,11 +166,11 @@ class App extends Component {
         <Particles className='particles'
           options={particlesOptions}
         />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         {route === 'home'
-          ?  <div>
+          ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -163,10 +178,10 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
           : (
-            route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
-            )          
+            route === 'signin'
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
