fb1750ec7defdcbc74d9152da45bf682912c5072
Marcos Lopes <marcos.eddul@gmail.com>
2022-02-19 13:09:25 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ import Rank from './components/Rank/Rank';
 import Particles from "react-tsparticles";
 import React, { Component } from 'react';
 import Clarifai from 'clarifai';
-
-
-const app = new Clarifai.App({
-  apiKey: '8ec97c58891449d29a169ebcd8e51449'
-});
-
+import { response } from 'express';
 
 const particlesOptions = {
   fpsLimit: 60,
@@ -135,8 +130,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
